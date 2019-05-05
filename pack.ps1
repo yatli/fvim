f3db8f3a900c94d264963c6bd59530a267750b79
Yatao Li <yatli@microsoft.com>
2019-05-05 21:22:45 +0800

    update packing script.

--- pack.ps1
+++ pack.ps1
@@ -1,10 +1,10 @@
-$plat = "win-x64","linux-x64","osx-x64"
-$ver  = git tag | Select-Object -First 1
+$plat = "win7-x64","win-x64","linux-x64","osx-x64"
 
 New-Item -ItemType Directory -Force -Name publish
+Remove-Item publish\*
 
 foreach($i in $plat) {
     dotnet publish -f netcoreapp2.1 -c Release --self-contained -r $i
-    Compress-Archive -Path bin/Release/netcoreapp2.1/$i/publish/* -DestinationPath publish/fvim-$ver-$i.zip -Force
+    Compress-Archive -Path bin/Release/netcoreapp2.1/$i/publish/* -DestinationPath publish/fvim-$i.zip -Force
 }
 
