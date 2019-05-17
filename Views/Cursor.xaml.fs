28ee2e49685160706083f3333d51e7e93df52d14
Yatao Li <yatli@microsoft.com>
2019-05-18 00:16:52 +0800

    optimize cursor move animation

--- Views/Cursor.xaml.fs
+++ Views/Cursor.xaml.fs
@@ -91,27 +91,29 @@ type Cursor() as this =
             let x_transition = DoubleTransition()
             x_transition.Property <- PosXProp
             x_transition.Duration <- TimeSpan.FromMilliseconds(100.0)
-            x_transition.Easing   <- Easings.ExponentialEaseOut()
+            x_transition.Easing   <- Easings.CubicEaseOut()
             let y_transition = DoubleTransition()
             y_transition.Property <- PosYProp
             y_transition.Duration <- TimeSpan.FromMilliseconds(100.0)
-            y_transition.Easing   <- Easings.ExponentialEaseOut()
+            y_transition.Easing   <- Easings.CubicEaseOut()
             transitions.Add(x_transition)
             transitions.Add(y_transition)
         this.SetValue(Cursor.TransitionsProperty, transitions)
 
     do
         AvaloniaXamlLoader.Load(this)
-        ignore <| Model.Notify "SetCursorAnimation" 
-            (function 
-             | [| Bool(blink) |] -> setCursorAnimation blink false
-             | [| Bool(blink); Bool(move) |] -> setCursorAnimation blink move
-             | _ -> setCursorAnimation false false
-        )
-        this.WhenActivated(fun disposables -> 
-            ignore <| this.GetObservable(PosXProp).Subscribe(fun x -> this.SetValue(Canvas.LeftProperty, x)).DisposeWith(disposables)
-            ignore <| this.GetObservable(PosYProp).Subscribe(fun y -> this.SetValue(Canvas.TopProperty, y)).DisposeWith(disposables)
-        ) |> ignore
+        [
+            Model.Notify "SetCursorAnimation" 
+                (function 
+                 | [| Bool(blink) |] -> setCursorAnimation blink false
+                 | [| Bool(blink); Bool(move) |] -> setCursorAnimation blink move
+                 | _ -> setCursorAnimation false false) 
+
+            this.WhenActivated(fun disposables -> 
+                ignore <| this.GetObservable(PosXProp).Subscribe(fun x -> this.SetValue(Canvas.LeftProperty, x)).DisposeWith(disposables)
+                ignore <| this.GetObservable(PosYProp).Subscribe(fun y -> this.SetValue(Canvas.TopProperty, y)).DisposeWith(disposables)
+            )
+        ] |> List.iter ignore
 
     override this.OnDataContextChanged _ =
         ignore <| this.GetObservable(RenderTickProp).Subscribe(cursorConfig)
