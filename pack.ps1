0617819e937643156ad9252b2d2ccc68acb52bdd
Yatao Li <yatli@microsoft.com>
2019-06-06 16:13:32 +0800

    ...

--- pack.ps1
+++ pack.ps1
@@ -2,6 +2,10 @@ $plat = "win7-x64","win-x64","linux-x64","osx-x64"
 
 New-Item -ItemType Directory -Force -Name publish
 Remove-Item publish\*
+Invoke-Command { 
+    nuget sources remove -Name "Avalonia Nightly"
+    nuget sources add -Name "Avalonia Nightly" -Source "https://www.myget.org/F/avalonia-ci/api/v2" -NonInteractive 
+} -ErrorAction SilentlyContinue
 
 foreach($i in $plat) {
     dotnet publish -f netcoreapp2.2 -c Release --self-contained -r $i
