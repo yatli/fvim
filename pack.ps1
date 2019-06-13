de16cd215c86e41700ec0091993326926b9fca62
Yatao Li <yatli@microsoft.com>
2019-06-13 19:35:32 +0800

    upgrade to netcoreapp3.0. emable assembly trimming

--- pack.ps1
+++ pack.ps1
@@ -1,6 +1,7 @@
 $plat = "win7-x64","win-x64","linux-x64","osx-x64"
 
-New-Item -ItemType Directory -Force -Name publish
+New-Item -ItemType Directory -Force -Name publish -ErrorAction SilentlyContinue
+Remove-Item -Recurse -Force bin\ -ErrorAction SilentlyContinue
 Remove-Item publish\*
 Invoke-Command { 
     nuget sources remove -Name "Avalonia Nightly"
@@ -8,11 +9,11 @@ Invoke-Command {
 } -ErrorAction SilentlyContinue
 
 foreach($i in $plat) {
-    dotnet publish -f netcoreapp2.2 -c Release --self-contained -r $i
+    dotnet publish -f netcoreapp3.0 -c Release --self-contained -r $i
     if ($i -eq "win-x64") {
 # replace the coreclr hosting exe with an icon-patched one
-        Copy-Item lib/fvim-win10.exe bin/Release/netcoreapp2.2/$i/publish/FVim.exe
+        Copy-Item lib/fvim-win10.exe bin/Release/netcoreapp3.0/$i/publish/FVim.exe
     }
-    Compress-Archive -Path bin/Release/netcoreapp2.2/$i/publish/* -DestinationPath publish/fvim-$i.zip -Force
+    Compress-Archive -Path bin/Release/netcoreapp3.0/$i/publish/* -DestinationPath publish/fvim-$i.zip -Force
 }
 
