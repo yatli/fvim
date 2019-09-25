935d944c8a72dea089b2611b406a72a3eaf7d3fd
Yatao Li <yatli@microsoft.com>
2019-09-25 17:48:33 +0800

    add crash report window

---
+++ ViewModels/CrashReportViewModel.fs
@@ -0,0 +1,23 @@
+namespace FVim
+
+open ui
+open log
+open common
+
+type CrashReportViewModel(ex: exn) as this =
+    inherit ViewModelBase()
+    member __.MainMessage =
+        ex.Message
+    member __.StackTrace =
+        ex.StackTrace.Split("\n")
+    member __.TipMessage = 
+        let tip = 
+            match ex.Message with
+            | "The system cannot find the file specified." -> "Tip: check your neovim installation. `nvim` is not in your $PATH.\n"
+            | _ -> ""
+        let generic_message = 
+               "You can go to https://github.com/yatli/fvim/issues, and search\n" + 
+               "for relevant issues with the exception message, or the stack trace.\n" +
+               "Feel free to create new issues, and I'll help to triage and fix the problem."
+        tip + generic_message
+
---
+++ Views/CrashReport.xaml.fs
@@ -0,0 +1,13 @@
+namespace FVim
+
+open ui
+open log
+open common
+open Avalonia.Markup.Xaml
+open Avalonia.Controls
+
+type CrashReport() as this =
+    inherit Window()
+
+    do
+        AvaloniaXamlLoader.Load(this)
