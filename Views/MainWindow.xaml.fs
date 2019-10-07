de0e1207b616c94a3cf063831b2eefe9d80193f1
Yatao Li <yatli@microsoft.com>
2019-10-07 23:27:21 +0800

    custom decoration: drag edges ok.

--- Views/MainWindow.xaml.fs
+++ Views/MainWindow.xaml.fs
@@ -9,13 +9,12 @@ open ReactiveUI
 open Avalonia.Markup.Xaml
 open Avalonia.Controls
 open Avalonia.Input
-open Avalonia.Interactivity
 open Avalonia
 open Avalonia.Data
-open Avalonia.VisualTree
 open Avalonia.ReactiveUI
 open System.Runtime.InteropServices
 open Avalonia.Rendering
+open Avalonia.Interactivity
 
 #nowarn "0025"
 
@@ -25,9 +24,22 @@ type MainWindow() as this =
     static let XProp = AvaloniaProperty.Register<MainWindow,int>("PosX")
     static let YProp = AvaloniaProperty.Register<MainWindow,int>("PosY")
 
-    let mutable m_saved_size  = Size(100.0,100.0)
-    let mutable m_saved_pos   = PixelPoint(300, 300)
-    let mutable m_saved_state = WindowState.Normal
+    let mutable m_saved_size          = Size(100.0,100.0)
+    let mutable m_saved_pos           = PixelPoint(300, 300)
+    let mutable m_saved_state         = WindowState.Normal
+    let mutable m_left_border:Panel   = null
+    let mutable m_right_border:Panel  = null
+    let mutable m_bottom_border:Panel = null
+
+    let m_cursor_ns = Cursor(StandardCursorType.SizeNorthSouth)
+    let m_cursor_we = Cursor(StandardCursorType.SizeWestEast)
+    let m_cursor_ne = Cursor(StandardCursorType.TopLeftCorner)
+    let m_cursor_nw = Cursor(StandardCursorType.TopRightCorner)
+    let m_cursor_se = Cursor(StandardCursorType.BottomLeftCorner)
+    let m_cursor_sw = Cursor(StandardCursorType.BottomRightCorner)
+
+    let setCursor c =
+        this.Cursor <- c
 
     let toggleFullscreen(v) =
         if not v then
@@ -57,6 +69,27 @@ type MainWindow() as this =
             this.HasSystemDecorations <- false
         else 
             this.HasSystemDecorations <- not this.ViewModel.Fullscreen
+
+    let dragThreshold = 5.0
+
+    let getDragEdge (pos: Point) =
+        if this.ViewModel.CustomTitleBar then
+            let l = pos.X <= dragThreshold
+            let r = this.Width - pos.X <= dragThreshold
+            let b = this.Height - pos.Y <= dragThreshold
+            let t = pos.Y <= dragThreshold
+            match l, r, b, t with
+            | true, _, false, false -> Some WindowEdge.West
+            | true, _, true, _      -> Some WindowEdge.SouthWest
+            | true, _, _, true      -> Some WindowEdge.NorthWest
+            | _, true, false, false -> Some WindowEdge.East
+            | _, true, true, _      -> Some WindowEdge.SouthEast
+            | _, true, _, true      -> Some WindowEdge.NorthWest
+            | _, _, true, _         -> Some WindowEdge.South
+            | _, _, _, true         -> Some WindowEdge.North
+            | _                     -> None
+        else None
+
 
     do
         #if DEBUG
@@ -95,9 +128,35 @@ type MainWindow() as this =
             this.AddHandler(DragDrop.DragOverEvent, (fun _ (e:DragEventArgs) ->
                 e.DragEffects <- DragDropEffects.Move ||| DragDropEffects.Link ||| DragDropEffects.Copy
             ))
+            this.AddHandler(MainWindow.PointerMovedEvent, (fun _ (ev: PointerEventArgs) ->
+                match getDragEdge <| ev.GetPosition(this) with
+                | Some (WindowEdge.NorthWest) -> setCursor m_cursor_nw
+                | Some (WindowEdge.SouthEast) -> setCursor m_cursor_se
+                | Some (WindowEdge.NorthEast) -> setCursor m_cursor_ne
+                | Some (WindowEdge.SouthWest) -> setCursor m_cursor_sw
+                | Some (WindowEdge.North | WindowEdge.South) -> setCursor m_cursor_ns
+                | Some (WindowEdge.East | WindowEdge.West) -> setCursor m_cursor_we
+                | _ -> setCursor Cursor.Default)
+                , RoutingStrategies.Tunnel)
+            this.AddHandler(MainWindow.PointerPressedEvent, (fun _ (ev: PointerPressedEventArgs) ->
+                match getDragEdge <| ev.GetPosition(this) with
+                | Some edge ->
+                    ev.Handled <- true
+                    this.BeginResizeDrag(edge)
+                | _ -> ())
+                , RoutingStrategies.Tunnel)
 
         ]
         AvaloniaXamlLoader.Load this
+
+    override __.OnPointerReleased ev =
+        setCursor Cursor.Default 
+        base.OnPointerReleased ev
+
+    override __.OnTemplateApplied _ =
+        m_left_border   <- this.FindControl<Panel>("LeftBorder")
+        m_right_border  <- this.FindControl<Panel>("RightBorder")
+        m_bottom_border <- this.FindControl<Panel>("BottomBorder")
 
     member this.GetEditor() =
         this.LogicalChildren.[0] :?> Avalonia.VisualTree.IVisual
