d69d25a550758ad91b1900d3f304ba4fbec46403
Yatao Li <yatli@microsoft.com>
2020-04-29 21:15:24 +0800

    update Avalonia to 0.9.9. Enable X11 immediate renderer

--- Program.fs
+++ Program.fs
@@ -26,7 +26,7 @@ let buildAvaloniaApp() =
     .UseReactiveUI()
     .With(new Win32PlatformOptions(UseDeferredRendering=false, AllowEglInitialization=true))
     .With(new AvaloniaNativePlatformOptions(UseDeferredRendering=false, UseGpu=true))
-    .With(new X11PlatformOptions(UseEGL=true, UseGpu=true))
+    .With(new X11PlatformOptions(UseDeferredRendering=false, UseEGL=true, UseGpu=true))
     .With(new MacOSPlatformOptions(ShowInDock=true))
     .LogToDebug()
 
