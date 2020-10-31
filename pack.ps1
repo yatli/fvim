1b1d888806fe72f8e35a84b5c8d2bc5e5822b5d8
Yatao Li <yatli@microsoft.com>
2020-10-31 14:44:14 +0800

    workaround Avalonia 0.10.0-preview6 packaging issues

--- pack.ps1
+++ pack.ps1
@@ -12,6 +12,8 @@ foreach($i in $plat) {
     if ($i -eq "win-x64") {
 # replace the coreclr hosting exe with an icon-patched one
         Copy-Item lib/fvim-win10.exe bin/Release/netcoreapp3.1/$i/publish/FVim.exe
+# Avalonia 0.10.0-preview6 fix: manually copy ANGLE from win7-x64
+        Copy-Item ~/.nuget/packages/avalonia.angle.windows.natives/2.1.0.2020091801/runtimes/win7-x64/native/av_libglesv2.dll bin/Release/netcoreapp3.1/$i/publish/
     } elseif ($i -eq "win7-x64") {
         Copy-Item lib/fvim-win7.exe bin/Release/netcoreapp3.1/$i/publish/FVim.exe
     }
