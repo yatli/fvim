8a7e6ffecf158c1e33fc11f0d2adc5a0737b6de6
Yatao Li <yatli@microsoft.com>
2021-09-28 00:27:00 +0800

    ...

--- update.ps1
+++ update.ps1
@@ -2,4 +2,5 @@ Stop-Process -Name "fvim"
 dotnet build -c Release fvim.fsproj
 cp bin/Release/net5.0/FVim* C:/tools/fvim/
 cp lib/fvim-win10.exe C:/tools/fvim/FVim.exe
+cp fvim.vim C:/tools/fvim/fvim.vim
 fvim
